8ea93bbc45b649247934af2e2dbc9789df786142
gruning-macos <alberto.gruning.zen@gmail.com>
2020-12-23 20:31:18 -0300

    cap9-connectWithGPIO

--- led_blink.py
+++ led_blink.py
@@ -16,17 +16,19 @@ GPIO.setup(butPin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 pwm = GPIO.PWM(pwmPin,200)
 GPIO.output(ledPin, GPIO.LOW)
 pwm.start(duty)
-
-while 1:
-    if GPIO.input(butPin):
-        pwm.ChangeDutyCycle(duty)
-        GPIO.output(ledPin,GPIO.LOW)
-    else:
-        pwm.ChangeDutyCycle(duty)
-        GPIO.output(ledPin,GPIO.HIGH)
-        time.sleep(0.5)
-        pwm.ChangeDutyCycle(100-duty)
-        GPIO.output(ledPin,GPIO.LOW)
-        time.sleep(0.5)
-
+try:
+    while 1:
+        if GPIO.input(butPin):
+            pwm.ChangeDutyCycle(duty)
+            GPIO.output(ledPin,GPIO.LOW)
+        else:
+            pwm.ChangeDutyCycle(duty)
+            GPIO.output(ledPin,GPIO.HIGH)
+            time.sleep(0.5)
+            pwm.ChangeDutyCycle(100-duty)
+            GPIO.output(ledPin,GPIO.LOW)
+            time.sleep(0.5)
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
     #button pressed # blink green # bright <->dim red
